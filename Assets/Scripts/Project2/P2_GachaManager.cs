a90ca84cfe243a05ce2ac6ca423d0a0b23419a81
NodaTakahiro039 <143977860+NodaTakahiro039@users.noreply.github.com>
2023-09-06 14:40:48 +0900

    Update P2_GachaManager.cs

--- Assets/Scripts/Project2/P2_GachaManager.cs
+++ Assets/Scripts/Project2/P2_GachaManager.cs
@@ -29,7 +29,7 @@ public class P2_GachaManager : MonoBehaviour
 
     public void OnClickGachaButton()
     {
-        gachaPickCharacterId = Random.Range(0, 6);
+        gachaPickCharacterId = Random.Range(0, DefineParam.CHARA_NUM);
 
         Debug.Log(gachaPickCharacterId.ToString());
 
